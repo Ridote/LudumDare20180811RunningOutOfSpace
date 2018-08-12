29bf3397a9bd93b9b6a7159c8ada8c8211b6604d
Jairo <kidandcat@gmail.com>
2018-08-12 15:21:33 +0200

    enemy

--- Entities/Enemies/Enemy.gd
+++ Entities/Enemies/Enemy.gd
@@ -1,18 +1,56 @@
 extends KinematicBody2D
 
-var allySpotted = false
+var playerSpotted = false
+var speed = 1
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	surveillance()
+	if $View.is_colliding():
+		if detect($View.get_collider().get_name()) == "player":
+			rotate_degrees(180)
+			playerSpotted = true
+	move()
+		
+func move():
+	var vel = Vector2(sin(self.rotation), -cos(self.rotation)).normalized()
+	if playerSpotted:
+		move_and_collide(vel * speed)
+	if playerSpotted:
+		move_and_collide(vel * speed * 2)
 
-func surveillance():
-	if !allySpotted:
-		var space_state = get_world_2d().direct_space_state
-		# use global coordinates, not local to node
-		var result = space_state.intersect_ray(Vector2(0, 0), Vector2(50, 100))
-
-func spotted(value = true):
-	allySpotted = true
+func rotate_degrees(degrees):
+	self.rotate(deg2rad(degrees))
+	
+func handleRotation():
+	if not $View3.is_colliding():
+		return;
+	if not $View2.is_colliding():
+		rotate(-45)
+		return;
+	if not $View4.is_colliding():
+		rotate(45)
+		return;
+	if not $View1.is_colliding():
+		rotate(-90)
+		return;
+	if not $View5.is_colliding():
+		rotate(90)
+		return;
+	if not $View6.is_colliding():
+		rotate(-135)
+		return;
+	if not $View7.is_colliding():
+		rotate(135)
+		return;
+	rotate(180)
+	
+func detect(name):
+	match name:
+		"Body", "Head":
+			return "player"
+		"Wall":
+			return "wall"
+		_: 
+			return "nothing"
