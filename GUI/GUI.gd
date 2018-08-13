ca2cacbe25261911bdc5ba887ed4828e8dc07e69
Jairo <kidandcat@gmail.com>
2018-08-13 13:42:12 +0200

    GUI manages enemies

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 var playerFactory = preload("res://Entities/Player/Player.tscn")
+var humanFactory = preload("res://Entities/Enemies/Enemy.tscn")
 
 func _ready():
 	$VBoxContainer/HBoxContainer2/Tired.region_enabled = false
@@ -20,7 +21,23 @@ func start():
 	player.global_position = get_parent().get_node("Map1").get_node("StartPosition").global_position
 	get_parent().add_child(player)
 	get_parent().get_node("Player").startTimer()
+	addEnemies()
 	$MainMenu.hide()
 	
 func showMainMenu():
+	removeEnemies()
 	$MainMenu.show()
+
+func addEnemies():
+	var children = get_parent().get_node("Map1").get_children()
+	for child in children:
+		if "HumanPosition" in child.get_name():
+			var human = humanFactory.instance()
+			human.global_position = child.global_position
+			get_parent().add_child(human)
+			
+func removeEnemies():
+	var children = get_parent().get_children()
+	for child in children:
+		if "Enemy" in child.get_name():
+			child.queue_free()
